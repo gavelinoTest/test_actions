c97760c41859d284ef4b066b15cf9127bff28733
Guilherme Avelino <gaa@ufpi.edu.br>
2022-08-23 15:46:51 -0300

    update project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Minha primeira aplicação React
         </p>
         <a
           className="App-link"
